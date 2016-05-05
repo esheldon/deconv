3b4e2ca495e4e6dddeff1d2063ed78da721bc384
Erin Sheldon <erin.sheldon@gmail.com>
2016-05-05 08:13:23 -0400

    support for more ngmix obs stuff

--- deconv/measure.py
+++ deconv/measure.py
@@ -9,40 +9,10 @@ from .deconv import DeConvolver
 
 from . import util
 
+MAX_ALLOWED_E=0.9999999
 
 LOW_T=2**0
-
-def calcmom_ksigma_obs(obs, sigma_weight, **kw):
-    """
-    Deconvolve the image and measure the moments using
-    a sigmak type weight
-
-    parameters
-    ----------
-    obs: ngmix Observation
-        An ngmix Observation, with a psf Observation set
-    sigma_weight: float
-        sigma for weight in real space, will be 1/sigma in k space.
-        If scale if image is not unity, the sigma will be
-        modified to be set to sigma*scale
-
-    returns
-    -------
-    A Moments object.  Use get_result() to get the result dict
-    """
-
-
-    image=obs.image
-    pimage=obs.psf.image
-    jac=obs.jacobian
-
-    wcs = jac.get_galsim_wcs()
-
-    gsim = galsim.Image(image.copy(), wcs=wcs)
-    psf_gsim = galsim.Image(pimage.copy(), wcs=wcs)
-
-    meas=calcmom_ksigma(gsim, psf_gsim, sigma_weight, **kw)
-    return meas
+HIGH_E=2**1
 
 def calcmom_ksigma(gal_image, psf_image, sigma_weight, **kw):
     """
@@ -93,6 +63,37 @@ def calcmom_ksigma(gal_image, psf_image, sigma_weight, **kw):
 
     return meas
 
+def calcmom_ksigma_obs(obs, sigma_weight, skip_flagged=True, **kw):
+    """
+    Deconvolve the image and measure the moments using
+    a sigmak type weight
+
+    parameters
+    ----------
+    obs: ngmix Observation
+        An ngmix Observation, ObsList, or MultiBandObsList,
+        with a psf Observations set
+    sigma_weight: float
+        sigma for weight in real space, will be 1/sigma in k space.
+        If scale if image is not unity, the sigma will be
+        modified to be set to sigma*scale
+    skip_flagged: bool
+        When averaging from multiple images, skip the ones with
+        flags set.  Default True.
+
+    **kw:
+        Keywords passed on to constuctors for KSigmaWeight and
+        DeConvolver, such as dk
+
+    returns
+    -------
+    An ObsKSigmaMoments object.  Use get_result() to get the result dict
+    """
+
+    meas=ObsKSigmaMoments(obs, sigma_weight, **kw)
+    meas.go()
+    return meas
+
 class Moments(object):
     """
     Deconvolve the psf and image and measure moments
@@ -124,9 +125,9 @@ class Moments(object):
         """
         get the result dictionary
         """
-        if not hasattr(self,'result'):
+        if not hasattr(self,'_result'):
             raise RuntimeError("run go() first")
-        return self.result
+        return self._result
 
     def go(self):
         """
@@ -138,21 +139,28 @@ class Moments(object):
 
         wim = self.kimage * self.kweight
 
-        imsum = wim.sum()
+        wimsum = wim.sum()
 
         irrsum_k = (rows**2*wim).sum()
         ircsum_k = (rows*cols*wim).sum()
         iccsum_k = (cols**2*wim).sum()
 
-        self._set_result(irrsum_k, ircsum_k, iccsum_k, imsum)
-
-    def _set_result(self, irrsum_k, ircsum_k, iccsum_k, imsum):
-
-        irr_k=irrsum_k/imsum
-        irc_k=ircsum_k/imsum
-        icc_k=iccsum_k/imsum
-
+        # put back on the natural scale
         dk=self.dk
+        dk2 = dk**2
+
+        irrsum_k *= dk2
+        ircsum_k *= dk2
+        iccsum_k *= dk2
+
+        self._set_result(irrsum_k, ircsum_k, iccsum_k, wimsum)
+
+    def _set_result(self, irrsum_k, ircsum_k, iccsum_k, wimsum):
+        from math import sqrt
+
+        irr_k=irrsum_k/wimsum
+        irc_k=ircsum_k/wimsum
+        icc_k=iccsum_k/wimsum
 
         T_k = irr_k + icc_k
 
@@ -164,14 +172,17 @@ class Moments(object):
         if T_k > 0:
 
             T=1.0/T_k
-            T /= dk**2
 
             e1 = -(icc_k - irr_k)/T_k
             e2 = -2.0*irc_k/T_k
+
+            etot = sqrt(e1**2 + e2**2)
+            if etot >= MAX_ALLOWED_E:
+                flags = HIGH_E
         else:
             flags=LOW_T
 
-        self.result={
+        self._result={
             'flags':flags,
 
             'e1':e1,
@@ -187,9 +198,9 @@ class Moments(object):
             'irrsum_k':irrsum_k,
             'ircsum_k':ircsum_k,
             'iccsum_k':iccsum_k,
-            'imsum':imsum,
+            'wimsum':wimsum,
 
-            'dk':dk,
+            'dk':self.dk,
         }
 
 
@@ -259,4 +270,137 @@ class Moments(object):
         """
         assert (im.shape[0] % 2)==0
         assert (im.shape[1] % 2)==0
+
+class ObsKSigmaMoments(Moments):
+    """
+    measure on ngmix Observation, ObsList, or MultiBandObsList
+
+    Use a fixed ksigma weight
+    """
+
+    def __init__(self, obs, sigma_weight, skip_flagged=True, **kw):
+        """
+        parameters
+        ----------
+        obs: ngmix observation type
+            an Observation, ObsList, or MultiBandObsList
+        """
+        import ngmix
+        self.mb_obs = ngmix.observation.get_mb_obs(obs)
+        self.sigma_weight=sigma_weight
+        self.skip_flagged=skip_flagged
+        self.kw=kw
+
+        # there is generally no single dk
+        if 'dk' in kw:
+            self.dk=kw['dk']
+        else:
+            self.dk=-9999.0
+
+    def get_result_list(self):
+        """
+        get the list of result dictionaries from all images
+        """
+        if not hasattr(self,'_reslist'):
+            raise RuntimeError("run go() first")
+        return self._reslist
+
+    def get_meas_list(self):
+        """
+        get the list of measureres
+        """
+        if not hasattr(self,'_measlist'):
+            raise RuntimeError("run go() first")
+        return self._measlist
+
+
+    def go(self):
+        """
+        measure moments on all images and perform the sum and mean
+        """
+        reslist=[]
+        measlist=[]
+
+        for obslist in self.mb_obs:
+            for obs in obslist:
+                tmeas=_calcmom_ksigma_obs(
+                    obs,
+                    self.sigma_weight,
+                    **self.kw
+                )
+                res=tmeas.get_result()
+
+                measlist.append(tmeas)
+                reslist.append(res)
+
+        self._measlist=measlist
+        self._reslist=reslist
+        self._combine_results()
+
+    def _combine_results(self):
+        flags=0
+
+        nimage_use   = 0
+        nimage_total = 0
+        irrsum_k     = 0.0
+        ircsum_k     = 0.0
+        iccsum_k     = 0.0
+        wimsum       = 0.0
+
+        for tres in self._reslist:
+            tflags=tres['flags']
+
+            nimage_total += 1
+
+            if tflags == 0 or not self.skip_flagged:
+                nimage_use += 1
+
+                irrsum_k += tres['irrsum_k']
+                ircsum_k += tres['ircsum_k']
+                iccsum_k += tres['iccsum_k']
+                wimsum   += tres['wimsum']
+
+                flags |= tflags
+
+        # now we can use the result setter in the parent
+        # class to finish the job
+
+        self._set_result(irrsum_k, ircsum_k, iccsum_k, wimsum)
+        res=self._result
+        res['nimage_use']   = nimage_use
+        res['nimage_total'] = nimage_total
+        res['orflags']      = flags
+
+def _calcmom_ksigma_obs(obs, sigma_weight, **kw):
+    """
+    Deconvolve the image and measure the moments using
+    a sigmak type weight
+
+    parameters
+    ----------
+    obs: ngmix Observation
+        An ngmix Observation, with a psf Observation set
+    sigma_weight: float
+        sigma for weight in real space, will be 1/sigma in k space.
+        If scale if image is not unity, the sigma will be
+        modified to be set to sigma*scale
+
+    returns
+    -------
+    A Moments object.  Use get_result() to get the result dict
+    """
+
+    image=obs.image
+    pimage=obs.psf.image
+    jac=obs.jacobian
+
+    wcs = jac.get_galsim_wcs()
+
+    gsim = galsim.Image(image.copy(), wcs=wcs)
+    psf_gsim = galsim.Image(pimage.copy(), wcs=wcs)
+
+    meas=calcmom_ksigma(gsim, psf_gsim, sigma_weight, **kw)
+
+    return meas
+
 
